d1564c12f64e15f0b682053d9bbb5d062acfaf49
Jimma Shanko <jshanko754@gmail.com>
2024-12-03 17:34:38 -0600

    Initial commit for CDK CloudFormation project

---
+++ test/cdk-corpweb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCorpweb from '../lib/cdk-corpweb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-corpweb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCorpweb.CdkCorpwebStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
